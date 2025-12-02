67bfa95aa4e72649a1554440b21df51f5ab6bb1f
kotopiro <mnxsv69789@yahoo.co.jp>
2025-12-02 21:23:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+// ←ここを追加
+app.get('/', (req, res) => {
+  res.send('ZCP Proxy Server is running!');
+});
+
+// 既存のプロキシルート
 app.get('/proxy', (req, res) => {
     const url = req.query.url;
     if (!url) return res.status(400).send('URL required');
+
     request(url).pipe(res);
 });
 
